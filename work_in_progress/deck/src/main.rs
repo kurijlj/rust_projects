c2870e17c3c78e7b26db23e0f64f917b7a0ff701
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2023-04-21 00:16:41 +0200

    G15 Dosimetry: 'Deck' class for the 'deck' subproject implemented. Next to implement command line arguments parsing supporitn options to select 'ordered' and 'shuffled' deck. And add documentation.

--- work_in_progress/deck/src/main.rs
+++ work_in_progress/deck/src/main.rs
@@ -1,50 +1,13 @@
 use std::fmt;
 use rand::Rng;
+use std::option::Option;
 
 fn main() {
-    let suites = [Suite::Clubs, Suite::Diamonds, Suite::Hearts, Suite::Spades];
-    let mut deck: Vec<Card> = Vec::new();
-    let mut stack_idxs: [u8; 52] = [0; 52];
+    let mut cs1 = Deck::new(false);
+    let mut cs2 = Deck::new(true);
 
-    let mut cntr: usize = 52;
-    while 0 < cntr {
-        let i: u8 = rand::thread_rng().gen_range(1..=52);
-        if !contains(&stack_idxs[..], i) {
-            stack_idxs[cntr-1] = i;
-            cntr = cntr - 1;
-        }
-    }
-
-    let mut i: usize = 1;
-    while 52 >= i {
-        let suite_idx = (stack_idxs[i-1] - 1) / 13;
-        let rank = ((stack_idxs[i-1] - 13*suite_idx) + 1) as u8;
-
-        deck.push(Card::new(&suites[suite_idx as usize], rank));
-
-        i = i + 1;
-    }
-
-    for card in deck {
-        println!("{}", card.to_string());
-    }
-
-}
-
-fn contains(a: &[u8], v: u8) -> bool {
-    if 1 == a.len() {
-        if a[0] == v {
-            true
-        }
-
-        else {
-            false
-        }
-    }
-
-    else {
-        let s: usize = (a.len() / 2) as usize;
-        contains(&a[0..s], v) || contains(&a[s..], v)
+    while !cs1.is_empty() {
+        println!("{}", cs1.pick_card().unwrap());
     }
 }
 
@@ -70,14 +33,28 @@ impl fmt::Display for Suite {
     }
 }
 
-pub struct Card<'a> {
-    suite: &'a Suite,
+fn get_suite(idx: usize) -> Option<Suite> {
+    if 4 > idx {
+        match idx {
+            0 => Some(Suite::Clubs),
+            1 => Some(Suite::Diamonds),
+            2 => Some(Suite::Hearts),
+            3 => Some(Suite::Spades),
+            _ => todo!(),
+        }
+    } else {
+        None
+    }
+}
+
+pub struct Card {
+    suite: Suite,
     rank: u8,
 }
 
 
-impl<'a> Card<'a> {
-    pub fn new(s: &Suite, r: u8) -> Card {
+impl Card {
+    pub fn new(s: Suite, r: u8) -> Card {
         Card {
             suite: s,
             rank: r,
@@ -85,7 +62,7 @@ impl<'a> Card<'a> {
     }
 }
 
-impl<'a> fmt::Display for Card<'a> {
+impl fmt::Display for Card {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let other_rank_str = &self.rank.to_string();
 
@@ -104,3 +81,58 @@ impl<'a> fmt::Display for Card<'a> {
     }
 }
 
+pub struct Deck {
+    cards: Vec<Card>,
+    shuffled: bool,
+}
+
+impl Deck {
+    pub fn new(shuffle: bool) -> Deck {
+        let mut stack: Vec<Card> = Vec::new();
+
+        let mut i: usize = 52;
+        while 1 <= i {
+            let suite_idx: usize = (i - 1)/13;
+            let rank: usize = (i - 13*suite_idx) + 1;
+
+            stack.push(Card::new(get_suite(suite_idx).unwrap(), rank as u8));
+
+            i = i - 1;
+        }
+
+        Deck {
+            cards: stack,
+            shuffled: shuffle,
+        }
+    }
+
+    pub fn shuffle(&mut self) {
+        self.shuffled = true;
+    }
+
+    pub fn pick_card(&mut self) -> Option<Card> {
+        if !self.is_empty() {
+            if self.shuffled {
+                let left = self.cards.len() - 1;
+                if 1 < left {
+                    let i = rand::thread_rng().gen_range(1..=left);
+                    Some(self.cards.remove(i))
+                } else {
+                    self.cards.pop()
+                }
+            } else {
+                self.cards.pop()
+            }
+        } else {
+            None
+        }
+    }
+
+    pub fn cards_left(&self) -> usize {
+        self.cards.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.cards.is_empty()
+    }
+}
